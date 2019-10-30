1e348c2796cde636c50b70234c939629563e6043
Kostoeva <rkostoeva@berkeley.edu>
2019-10-29 17:58:27 -0700

    adding oclor picker asset

---
+++ Assets/Color_Picker/Scripts/Draggable.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+	public bool fixX;
+	public bool fixY;
+	public Transform thumb;	
+	bool dragging;
+
+    private GameObject rightController;
+    private void Start()
+    {
+        rightController = GameObject.Find("CustomHandRight");
+    }
+
+    void FixedUpdate()
+	{
+		if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger)) {
+			dragging = false;
+            Ray ray = new Ray(rightController.transform.position, rightController.transform.forward);
+			RaycastHit hit;
+			if (GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity)) {
+				dragging = true;
+			}
+		}
+		if (OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger)) dragging = false;
+		if (dragging && OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger)) {
+            Ray ray = new Ray(rightController.transform.position, rightController.transform.forward);
+            RaycastHit hit;
+            if (GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
+            {
+                var point = hit.point;
+                point = GetComponent<Collider>().ClosestPointOnBounds(point);
+                SetThumbPosition(point);
+                SendMessage("OnDrag", Vector3.one - (thumb.position - GetComponent<Collider>().bounds.min) / GetComponent<Collider>().bounds.size.x);
+            }
+            
+		}
+	}
+
+	void SetDragPoint(Vector3 point)
+	{
+		point = (Vector3.one - point) * GetComponent<Collider>().bounds.size.x + GetComponent<Collider>().bounds.min;
+		SetThumbPosition(point);
+	}
+
+	void SetThumbPosition(Vector3 point)
+	{
+		thumb.position = new Vector3(fixX ? thumb.position.x : point.x, fixY ? thumb.position.y : point.y, thumb.position.z);
+	}
+}
